aa6f2f6e8e3a1e39e8db70ad16c6967381b6281b
Sam Hamm <samhamm@gmail.com>
2017-02-21 09:34:40 -0800

    ready to go

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # code-301-github-api
+
 Class 12 demo for Code 301 (accessing the GitHub API)
---
+++ public/app.js
@@ -0,0 +1,8 @@
+// Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
+
+$.ajax({
+  // go get the data
+})
+.then(
+  // render the data
+  });
