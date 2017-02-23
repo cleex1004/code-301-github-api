ddd246e90a9381b0a12270bc35347ad16998e04c
cleex1004 <cleex1004@gmail.com>
2017-02-22 16:03:02 -0800

    token done

--- public/app.js
+++ public/app.js
@@ -1,17 +1,36 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
 
-$.ajax({
-	url:'https://api.github.com/user/repos?type=owner',
-	method: 'GET',
-	headers: {
-		Authorization: process.env.GITHUB_TOKEN}
-		//Authorization: githubToken}
-    //Authorization: `token ${githubToken}`}
-})
+$.get('github/user/repos')
 .then(data => {
   console.log(data);
   data.forEach(repo => $('#results').append(`<p>${repo.name}</p>`));
 },
   err => {
-    console.error(err);
+    console.error('Status Code: ', err.status);
   });
+
+// $.get('/github/user/repos')
+// .then(data => {
+//   console.log(data);
+//   data.forEach(repo => $('#results').append(`<p>${repo.name}</p>`));
+// }, err => {
+//     console.error('status code: ', err.status);
+//   });
+
+	// $.ajax({
+	// 	//url:'https://api.github.com/user/repos?type=owner',
+	// 	url:'/github/user/repos',
+	// 	method: 'GET',
+	// 	headers: {
+	// 		Authorization:
+	// 	}
+	// 		//Authorization: githubToken}
+	//     //Authorization: `token ${githubToken}`}
+	// })
+	// .then(data => {
+	//   console.log(data);
+	//   data.forEach(repo => $('#results').append(`<p>${repo.name}</p>`));
+	// },
+	//   err => {
+	//     console.error(err);
+	//   });
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// Added the following line to bring in the dependency
+const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -7,5 +9,24 @@ app.use(express.static('./public'));
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 });
+
+// handles the route for our AJAX request to GitHub
+// proxyGitHub is a callback
+app.get('/github/*', proxyGitHub);
+
+function proxyGitHub(request, response) {
+  console.log('Routing a GitHub request for ', request.params[0]); //.params[0] contains whatever the * represents in the route
+  (requestProxy({  //this is an IIFE, otherwise it will NOT run
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+// function proxyGitHub(request, reponse) {
+//   console.log('Routing a GitHub request for ', request.params[0]);
+//   (requestProxy({
+//     url: `https://api.github.com/${request.params[0]}`,
+//     headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+//   }))(request, response);
+// }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
